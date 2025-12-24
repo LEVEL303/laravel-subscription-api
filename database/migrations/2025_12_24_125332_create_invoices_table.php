79f96bb62dbd2aaf45560d1d11bd7c6a423e0eda
Levitico Rimon <leviticorimon@gmail.com>
2025-12-24 15:01:13 -0300

    feat(database): cria estrutura de migrations

---
+++ database/migrations/2025_12_24_125332_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('subscription_id')->constrained();
+            $table->string('gateway_id')->nullable();
+            $table->integer('amount');
+            $table->string('status');
+            $table->string('payment_method')->nullable();
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamp('due_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
